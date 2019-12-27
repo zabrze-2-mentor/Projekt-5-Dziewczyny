b269f4b39ef2d6ff431e8fb1800f73c4c8b1d6d2
Aleksandra Fabian <fabianaleksandra@gmail.com>
2019-12-27 18:04:44 +0100

    Style komponentów Modal oraz Button

--- cinema/src/components/Modal/Modal.js
+++ cinema/src/components/Modal/Modal.js
@@ -6,16 +6,39 @@ const Modal = (props) => {
         <div>
             <div className="modal_wrapper"
                 style={{
-                    transform: props.show ? 'translateY(0vh)' : 'translateY(-100vh)',
+                    transform: props.show ? 'translateY(0vh)' : 'translateY(-10vh)',
                     opacity: props.show ? '1' : '0'
                 }}>
-
                 <span className="close_modal_btn" onClick={props.close}>×</span>
+                <div className="modal_header">
+                    <h1>Bardzo długaśny tytuł jakiegoś nudnego filmu o czymś ciekawym</h1>
+                </div>
 
                 <div className="modal_body">
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
+                    <div className="info">
+                        <img src="https://ocs-pl.oktawave.com/v1/AUTH_2887234e-384a-4873-8bc5-405211db13a2/splay/2019/11/harry-potter-kamie%C5%84-filozoficzny-premiera.jpg" />
+                        <div className="info_wrapper">
+                            <div className="det">
+                                <h6><span>Gatunek: </span><br />Horror</h6>
+                                <h6><span>Rok produkcji: </span><br />2019</h6>
+                                <h6><span>Reżyser: </span><br />Imię i Nazwisko</h6>
+                                <h6><span>Kraj produkcji: </span><br />Kraj</h6>
+                            </div>
+                            <div className="descr">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nis ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nis ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nis ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                    laboris nis ut aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
