96362ec818fa38a8919fcb4e13f0d77e59e891e7
Aleksandra Fabian <fabianaleksandra@gmail.com>
2019-12-27 02:18:31 +0100

    Stworzenie komponentu Modal

--- cinema/src/components/App/App.js
+++ cinema/src/components/App/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
+import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
 
-function App() {
-  return (
-    <h1>
-      U mnie działa
-    </h1>
-  );
+class App extends React.Component {
+
+  state = {
+    isShowing: false
+  }
+
+  openModalHandler = () => {
+    this.setState({
+      isShowing: true
+    });
+  }
+
+  closeModalHandler = () => {
+    this.setState({
+      isShowing: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          U mnie działa
+      </h1>
+        <Button
+          type="Button"
+          onClick={this.openModalHandler}
+          buttonStyle="btn_blue"
+        >Temporary modal button</Button>
+        <Modal
+          className="modal"
+          show={this.state.isShowing}
+          close={this.closeModalHandler}>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
